bee03702b7231136ef9977e836727c88774fffa2
Ivan Jaimes <ivan.iver@gmail.com>
2014-12-03 18:57:09 -0600

    Improve result message

--- update.go
+++ update.go
@@ -23,22 +23,22 @@ func main() {
 
 	list, _ := ioutil.ReadDir(*dir)
 	for _, file := range list {
-		if err := UpdateName(file, *dir, &serie); err != nil {
+		if new_name, err := UpdateName(file, *dir, &serie); err != nil {
 			fmt.Println("Error: %v", err)
 		} else {
-			fmt.Printf("File %v renamed", file.Name())
+			fmt.Printf("\n File %v renamed to %v", file.Name(), new_name)
 		}
 	}
 }
 
-func UpdateName(file os.FileInfo, path string, serie *Serie) (err error) {
+func UpdateName(file os.FileInfo, path string, serie *Serie) (new_name string, err error) {
 	oldname := file.Name()
 	name := strings.SplitAfter(oldname, "_")
 	serie.Next()
 
 	oldname = fmt.Sprintf("%v/%v", path, oldname)
 	filename := fmt.Sprintf("%v/%v_%v", path, serie.Formated, name[1])
-	return os.Rename(oldname, filename)
+	return filename, os.Rename(oldname, filename)
 }
 
 func NewSerie(init_time time.Time) (serie Serie, err error) {
